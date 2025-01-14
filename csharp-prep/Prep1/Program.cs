e57923f3271ba2f2002935e2a84a6f8ac656729a
nniumei <nnygtz@byui.edu>
2025-01-13 17:27:57 -0700

    git push origin main

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.Write($"Hello, {firstName} {lastName}! How are you today? ");
+        string response = Console.ReadLine();
+
+        Console.WriteLine($"You said that {response}. You know I'm just a computer program, right? I have no time for menial small talk.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your current grade percentage in this class? Just give me an estimate.");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed! Good stuff!");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("It's not close enough, you just lost out on those credits.");
+        }
+        else
+        {
+            Console.WriteLine("Be better next time, okay? Can't have you wasting your tuition on poor performance.");
+        }
     }
 }
